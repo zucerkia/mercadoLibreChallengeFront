10667adeaedb0f4cd881f8d0cefe160f749436d4
julian.sucerquia <julian.sucerquia@pragma.com.co>
2022-08-21 13:06:24 -0500

    create initial routing

---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const HomePage = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
+
+export default HomePage
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './components/organisms/App/App'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+
+// Components
+import HomePage from './components/pages/Home'
+import ItemsPage from './components/pages/Items'
+import DetailPage from './components/pages/Detail'
+
+// Styles
+import './index.css'
+import PageTemplate from './components/templates/PageTemplate'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <PageTemplate>
+        <Link to="items">items</Link>
+        <Link to="items/23">items/id</Link>
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route path="/items" element={<ItemsPage />}>
+              <Route path=":id" element={<DetailPage />} />
+            </Route>
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>404 page not found</p>
+                </main>
+              }
+            />
+          </Route>
+        </Routes>
+      </PageTemplate>
+    </Router>
+    <HomePage />
   </React.StrictMode>
 )
 
